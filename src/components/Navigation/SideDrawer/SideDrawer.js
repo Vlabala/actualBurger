cfbc7f8266dde16737b413bdac095694c26f94a7
Vlabala <Vlabalala@gmail.com>
2019-06-14 01:11:56 +0300

    Menu and shit

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import classes from './SideDrawer.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxs from '../../../hoc/Auxs';
+
+const sideDrawer = props => {
+    let attachedClasses = [classes.SideDrawer,classes.Close];
+    if(props.open){
+        attachedClasses = [classes.SideDrawer,classes.Open];
+
+    }
+    return (
+        <Auxs>
+            <Backdrop clicked={ props.closed } show={ props.open }/>
+            <div className={ attachedClasses.join(' ') }>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
+            </div>
+        </Auxs>
+    );
+
+};
+
+export default sideDrawer;
